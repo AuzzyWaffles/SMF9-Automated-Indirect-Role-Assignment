aa603ff83a2ed074aa0a760be9d6f8c1c0d6b025
AuzzyWaffles <galleaus@amazon.com>
2025-02-28 17:30:53 -0800

    commiting to work on at home

--- display_manager.py
+++ display_manager.py
@@ -1,11 +1,11 @@
 import datetime as dt
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, simpledialog
 import file_path
 from assignment_manager import AssignmentManager
 import permissions_manager as pm
 from schedule_finder import get_scheduled_associates
-from customization_manager import change_site, change_shifts, add_remove_roles
+import customization_manager as custom
 
 
 class DisplayManager:
@@ -14,8 +14,7 @@ class DisplayManager:
         with open(file_path.get_custom_text('site'), "r") as file:
             self.site = file.read().upper()
 
-        with open(file_path.get_custom_text('saved_shifts'), 'r') as file:
-            self.shifts = [shift[:-1] for shift in file.readlines()]
+        self.shifts = None
 
         self.current_time = dt.datetime.now()
         self.year = self.current_time.strftime('%Y')
@@ -29,6 +28,7 @@ class DisplayManager:
         self.center_window(self.window, 500, 600)
 
         self.text_window = None
+        self.text_box = None
         self.lines = None
         self.entry_dict = None
 
@@ -42,45 +42,47 @@ class DisplayManager:
         self.canvas.bind_all("<MouseWheel>", self.on_mouse_wheel)
         self.canvas.bind("<Configure>", lambda event, canvas=self.canvas: self.on_frame_configure())
 
-        self.text = self.canvas.create_text(260, 60,
-                                            text='Koality Rotator',
-                                            font=("Helvetica", 20, "bold"))
+        # self.text = self.canvas.create_text(260, 60,
+        #                                     text='Koality Rotator',
+        #                                     font=("Helvetica", 20, "bold"))
+        #
+        # self.dropdown_text = self.canvas.create_text(75, 100, text='Select a Shift:', font=("Helvetica", 12, "bold"))
+        self.dropdown_widget = None
+        # self.dropdown_widget.set("Select an option")
+        # self.canvas.create_window(250, 100, window=self.dropdown_widget)
 
-        self.dropdown_text = self.canvas.create_text(75, 100, text='Select a Shift:', font=("Helvetica", 12, "bold"))
-        self.dropdown_widget = ttk.Combobox(self.window, values=self.shifts)
-        self.dropdown_widget.set("Select an option")
-        self.canvas.create_window(250, 100, window=self.dropdown_widget)
+        self.site_text = None
 
-        self.site_text = self.canvas.create_text(435, 100, text=f'Site: {self.site}', font=("Helvetica", 12, "bold"))
-
-        self.date_text = self.canvas.create_text(73, 132, text='Type a Date:', font=("Helvetica", 12, "bold"))
+        # self.date_text = self.canvas.create_text(73, 132, text='Type a Date:', font=("Helvetica", 12, "bold"))
         self.date_entry = ttk.Entry(width=23)
         self.date_entry.insert(0, string=f'{self.month}-{self.day_number}-{self.year}')
-        self.canvas.create_window(250, 132, window=self.date_entry)
+        # self.canvas.create_window(250, 132, window=self.date_entry)
 
         style = ttk.Style()
         style.configure('TButton', padding=(0, 35, 0, 32), font=('Helvetica', 10))
 
-        self.change_site_button = tk.Button(text="Change Site", width=10, command=lambda: change_site(self))
-        self.change_shifts_button = tk.Button(text='Change Shifts', width=10, command=lambda: change_shifts(self))
+        self.change_site_button = tk.Button(text="Change Site", width=10, command=self.change_site)
+        self.change_shifts_button = tk.Button(text='Change Shifts', width=10, command=self.change_shifts)
         self.generate_button = ttk.Button(text="Generate", width=35, command=self.generate_button_click)
         self.permissions_button = ttk.Button(text="Check/Edit Permissions", width=35,
-                                             command=lambda: pm.check_permissions(self))
+                                             command=self.check_permissions)
         self.add_remove_button = tk.Button(text='Add/Remove Roles',
-                                           command=lambda: add_remove_roles(self))
+                                           command=self.add_remove_roles)
 
-        self.canvas.create_window(435, 132, window=self.change_site_button)
-        self.canvas.create_window(435, 165, window=self.change_shifts_button)
-        self.canvas.create_window(250, 200, window=self.generate_button)
-        self.canvas.create_window(250, 385, window=self.permissions_button)
-        self.canvas.create_window(410, 465, window=self.add_remove_button)
+        # self.canvas.create_window(435, 132, window=self.change_site_button)
+        # self.canvas.create_window(435, 165, window=self.change_shifts_button)
+        # self.canvas.create_window(250, 200, window=self.generate_button)
+        # self.canvas.create_window(250, 385, window=self.permissions_button)
+        # self.canvas.create_window(410, 465, window=self.add_remove_button)
 
-        self.canvas.create_line(0, 445, 500, 445, fill="black", width=2)
+        # self.canvas.create_line(0, 445, 500, 445, fill="black", width=2)
 
         self.smile_image1 = tk.PhotoImage(file=file_path.get_logo())
-        self.canvas.create_image(250, 285, image=self.smile_image1, tag='image1')
+        # self.canvas.create_image(250, 285, image=self.smile_image1, tag='image1')
 
-        self.generate_roles()
+        self.generate_canvas()
+        self.canvas.pack()
+        self.canvas.mainloop()
 
     def center_window(self, window, width, height):
         # Get the screen's width and height
@@ -106,8 +108,28 @@ class DisplayManager:
         if event.delta:
             self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
-    def generate_roles(self):
+    def generate_canvas(self):
         """Reads and creates text/entry for every saved role"""
+        self.canvas.create_text(260, 60,
+                                            text='Koality Rotator',
+                                            font=("Helvetica", 20, "bold"))
+        self.canvas.create_text(75, 100, text='Select a Shift:', font=("Helvetica", 12, "bold"))
+        self.shifts = custom.get_shifts().split('\n')
+        self.shifts.pop()
+        self.dropdown_widget = ttk.Combobox(self.window, values=self.shifts)
+        self.dropdown_widget.set("Select an option")
+        self.canvas.create_window(250, 100, window=self.dropdown_widget)
+        self.site_text = self.canvas.create_text(435, 100, text=f'Site: {self.site}', font=("Helvetica", 12, "bold"))
+        self.canvas.create_text(73, 132, text='Type a Date:', font=("Helvetica", 12, "bold"))
+        self.canvas.create_window(250, 132, window=self.date_entry)
+        self.canvas.create_window(435, 132, window=self.change_site_button)
+        self.canvas.create_window(435, 165, window=self.change_shifts_button)
+        self.canvas.create_window(250, 200, window=self.generate_button)
+        self.canvas.create_window(250, 385, window=self.permissions_button)
+        self.canvas.create_window(410, 465, window=self.add_remove_button)
+        self.canvas.create_line(0, 445, 500, 445, fill="black", width=2)
+        self.canvas.create_image(250, 285, image=self.smile_image1, tag='image1')
+
         with open(file_path.get_custom_text('saved_roles'), "r") as file:
             self.lines = [line.strip() for line in file.readlines()]
             curr_y = 465
@@ -124,6 +146,111 @@ class DisplayManager:
 
         self.canvas.pack()
         self.canvas.mainloop()
+
+
+    def change_site(self):
+        try:
+            self.site = simpledialog.askstring('Change Site', 'Please enter your site:').upper()
+            custom.change_site(self.site)
+            self.canvas.delete(self.site_text)
+            self.site_text = self.canvas.create_text(435, 100, text=f'Site: {self.site}',
+                                                     font=("Helvetica", 12, "bold"))
+        except AttributeError:
+            pass
+
+    def change_shifts(self):
+        messagebox.showinfo(title="Shifts Input", message="Please ensure all shifts are entered as follows:\nHH-MM-00")
+        shifts = custom.get_shifts()
+
+        self.text_window = tk.Tk()
+        self.text_window.title('Change Shifts')
+        self.text_window.iconbitmap(file_path.get_koala())
+        self.text_box = tk.Text(self.text_window)
+        self.text_box.insert(tk.END, chars=shifts)
+
+        save_button = ttk.Button(self.text_window, text="Save", width=20,
+                                 command=lambda: self.__save('saved_shifts'))
+        save_button.place(x=480, y=10)
+
+        # Create a vertical scrollbar
+        scrollbar = tk.Scrollbar(self.text_window, orient="vertical", command=self.text_box.yview)
+        scrollbar.pack(side="right", fill="y")
+
+        # Configure the Text widget to use the scrollbar
+        self.text_box.config(yscrollcommand=scrollbar.set)
+
+        self.text_box.pack()
+        self.center_window(self.text_window, 630, 300)
+        self.text_window.mainloop()
+
+    def add_remove_roles(self):
+        roles = custom.add_remove_roles()
+        self.text_window = tk.Tk()
+        self.text_window.title("Add/Remove Roles")
+        self.text_window.iconbitmap(file_path.get_koala())
+        self.text_box = tk.Text(self.text_window)
+        self.text_box.insert(tk.END, chars=roles)
+
+        save_button = ttk.Button(self.text_window, text="Save", width=20,
+                                 command=lambda: self.__save('saved_roles'))
+        save_button.place(x=480, y=10)
+
+        # Create a vertical scrollbar
+        scrollbar = tk.Scrollbar(self.text_window, orient="vertical", command=self.text_box.yview)
+        scrollbar.pack(side="right", fill="y")
+
+        # Configure the Text widget to use the scrollbar
+        self.text_box.config(yscrollcommand=scrollbar.set)
+
+        self.text_box.pack()
+        self.center_window(self.text_window, 630, 300)
+        self.text_window.mainloop()
+
+    def __save(self, filename):
+        text = self.text_box.get("1.0", 'end-1c').split('\n')
+        text = [item for item in text if item != '']
+        custom.save(filename, text)
+
+        self.text_window.destroy()
+        tk.messagebox.showinfo(title="Saved!",
+                               message='Saved!')
+
+        self.canvas.delete('all')
+        self.generate_canvas()
+
+    def check_permissions(self):
+        permissions_string = pm.check_permissions()
+
+        self.text_window = tk.Tk()
+        self.text_window.title("Check/Edit Permissions")
+        self.text_window.iconbitmap(file_path.get_koala())
+        text = tk.Text(self.text_window)
+        text.insert(tk.END, chars=permissions_string)
+
+        save_button = ttk.Button(self.text_window, text="Save", width=20,
+                                 command=lambda: self.__save_permissions(text))
+        save_button.place(x=480, y=10)
+
+        # Create a vertical scrollbar
+        scrollbar = tk.Scrollbar(self.text_window, orient="vertical", command=text.yview)
+        scrollbar.pack(side="right", fill="y")
+
+        # Configure the Text widget to use the scrollbar
+        text.config(yscrollcommand=scrollbar.set)
+
+        text.pack()
+        self.center_window(self.text_window, 630, 300)
+        self.text_window.mainloop()
+
+    def __save_permissions(self, text):
+        saved = pm.save_permissions(text)
+        self.text_window.destroy()
+        if not saved:
+            tk.messagebox.showinfo(title="Error",
+                                   message='Error: Changes not saved.\n\nIf you need to add or remove roles, '
+                                           'please do so on the main menu by clicking "Add/Remove Roles."')
+        else:
+            tk.messagebox.showinfo(title="Saved!", message='Permissions Saved!')
 
     def generate_button_click(self):
         all_zero = True
--- permissions_manager.py
+++ permissions_manager.py
@@ -1,49 +1,23 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
 import file_path
 
 with open(file_path.get_custom_text('saved_roles'), "r") as file:
     indirect_roles = [line.strip() for line in file.readlines()]
 
-    def check_permissions(display):
+    def check_permissions():
         permissions_string = ""
 
         for role in indirect_roles:
             with open(file_path.get_permissions(role), "r") as file:
                 text = file.read()
                 permissions_string += text
-
-        display.text_window = tk.Tk()
-        display.text_window.title("Check/Edit Permissions")
-        display.text_window.iconbitmap(file_path.get_koala())
-        text = tk.Text(display.text_window)
-        text.insert(tk.END, chars=permissions_string)
-
-        save_button = ttk.Button(display.text_window, text="Save", width=20,
-                                 command=lambda: __save_permissions(text, display))
-        save_button.place(x=480, y=10)
-
-        # Create a vertical scrollbar
-        scrollbar = tk.Scrollbar(display.text_window, orient="vertical", command=text.yview)
-        scrollbar.pack(side="right", fill="y")
-
-        # Configure the Text widget to use the scrollbar
-        text.config(yscrollcommand=scrollbar.set)
-
-        text.pack()
-        display.center_window(display.text_window, 630, 300)
-        display.text_window.mainloop()
+        return permissions_string
 
 
-    def __save_permissions(text, display):
+    def save_permissions(text):
         saved_permissions = text.get("1.0", 'end-1c').split("##")
 
         if len(saved_permissions) - 1 != len(indirect_roles):
-            tk.messagebox.showinfo(title="Error",
-                                   message='Error: Changes not saved.\n\nIf you need to add or remove roles, '
-                                           'please do so on the main menu by clicking "Add/Remove Roles."')
-            display.text_window.destroy()
-            return
+            return False
         else:
             for n in range(1, len(saved_permissions)):
                 saved_permissions[n] = f"##{saved_permissions[n]}"
@@ -52,9 +26,7 @@ with open(file_path.get_custom_text('saved_roles'), "r") as file:
             for role in indirect_roles:
                 with open(file_path.get_permissions(role), "w") as file:
                     file.write(next(saved_permissions_iter))
-
-        display.text_window.destroy()
-        tk.messagebox.showinfo(title="Saved!", message='Permissions Saved!')
+            return True
 
     def get_permissions():
         permissions_dict = {}
