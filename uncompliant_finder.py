8d6562fa154c45a2d72b975da35de1e73a9b41f2
AuzzyWaffles <galleaus@amazon.com>
2025-03-03 13:51:21 -0800

    Removed Class UncompliantFinder

--- uncompliant_finder.py
+++ uncompliant_finder.py
@@ -1,21 +1,19 @@
 import os
 import pandas as pd
 
+file_name = 'Processing Compliance.xlsm'
+desktop_path = os.path.expanduser('~\\Desktop')
+file_path = os.path.join(desktop_path, file_name)
 
-class UncompliantFinder:
-    def __init__(self):
-        self.file_name = 'Processing Compliance.xlsm'
-        self.desktop_path = os.path.expanduser('~\\Desktop')
-        self.file_path = os.path.join(self.desktop_path, self.file_name)
 
-    def get_uncompliant_processors(self):
-        df = pd.read_excel(self.file_path, sheet_name='Processing Compliance')
+def get_uncompliant_processors():
+    df = pd.read_excel(file_path, sheet_name='Processing Compliance')
 
-        df.loc[0, 'Compliance'] = 0
-        pd.to_numeric(df['Compliance'])
+    df.loc[0, 'Compliance'] = 0
+    pd.to_numeric(df['Compliance'])
 
-        uncompliant_rows = df[df['Compliance'] > 0]
-        uncompliant_processors = uncompliant_rows['Unnamed: 1']
-        uncompliant_processors = {associate for associate in uncompliant_processors}
+    uncompliant_rows = df[df['Compliance'] > 0]
+    uncompliant_processors = uncompliant_rows['Unnamed: 1']
+    uncompliant_processors = {associate for associate in uncompliant_processors}
 
-        return uncompliant_processors
+    return uncompliant_processors
