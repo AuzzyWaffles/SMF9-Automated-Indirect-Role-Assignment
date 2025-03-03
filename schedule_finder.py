1cd9ab2dc870a0e29b7bc0299ed1b16a399c6886
AuzzyWaffles <galleaus@amazon.com>
2025-03-03 13:47:46 -0800

    Optimized Selenium by replacing Xpaths

--- schedule_finder.py
+++ schedule_finder.py
@@ -31,26 +31,23 @@ def get_scheduled_associates(display, shift, date):
         driver.get(f'https://sspot.iad.corp.amazon.com/{display.site}/schedule-timeline')
 
         # Wait for Midway to load first
-        wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="user_name"]')))
+        wait.until(ec.visibility_of_element_located((By.ID, 'user_name')))
 
         # Enter username based on OS Login
-        driver.find_element(By.XPATH, '//*[@id="user_name"]').send_keys(os.getlogin())
+        driver.find_element(By.ID, 'user_name').send_keys(os.getlogin())
 
         pin = display.midway_pin()
-        driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(pin)
+        driver.find_element(By.ID, 'password').send_keys(pin)
 
         # Enter One-Time Password via Security Key
         otp = display.security_key()
-        driver.find_element(By.XPATH, '//*[@id="otp"]').send_keys(otp)
-        driver.find_element(By.XPATH, '//*[@id="verify_btn"]').click()
-
-        # Wait for SSPOT to load
-        wait.until(ec.visibility_of_element_located((By.XPATH, '/html/body/div/div/nav/a/img')))
+        driver.find_element(By.ID, 'otp').send_keys(otp)
+        driver.find_element(By.ID, 'verify_btn').click()
 
         # Enter the date that was entered by the user, have to do it twice
-        wait.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="date-picker"]')))
+        wait.until(ec.visibility_of_element_located((By.ID, 'date-picker')))
         for _ in range(2):
-            date_entry = driver.find_element(By.XPATH, '//*[@id="date-picker"]')
+            date_entry = driver.find_element(By.ID, 'date-picker')
             date_entry.send_keys(month_num)
             if month_num == 1:
                 date_entry.send_keys(Keys.TAB)
@@ -61,7 +58,7 @@ def get_scheduled_associates(display, shift, date):
 
             date_entry.send_keys(year)
 
-            driver.find_element(By.XPATH, '//*[@id="schedule-timeline-current-week-text"]').click()
+            driver.find_element(By.ID, 'schedule-timeline-current-week-text').click()
 
         # Find and click the relevant shift based on current datetime values
         wait.until(
@@ -70,29 +67,24 @@ def get_scheduled_associates(display, shift, date):
 
         driver.execute_script('window.scrollTo(0, 0);')
 
-        wait_long.until(ec.visibility_of_element_located((By.XPATH, '//*[@id="loading-bar"]/div[1]/div[1]/button')))
-        attribute_button = driver.find_element(By.XPATH, '//*[@id="loading-bar"]/div[1]/div[1]/button')
+        wait_long.until(ec.visibility_of_element_located((By.CLASS_NAME, 'display-attribute')))
+        attribute_button = driver.find_element(By.CLASS_NAME, 'display-attribute')
         driver.execute_script('arguments[0].scrollIntoView(true);', attribute_button)
         attribute_button.click()
 
         # Uncheck boxes on attribute list
         wait.until(
-            ec.visibility_of_element_located((By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[1]/input')))
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[1]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[4]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[6]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[7]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[8]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[10]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[11]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[12]/input').click()
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox"]/div[13]/input').click()
+            ec.visibility_of_element_located((By.ID, 'roster-details-multi-checkbox')))
+        checkboxes = driver.find_elements(By.CSS_SELECTOR, '#roster-details-multi-checkbox div')
+        for i, element in enumerate(checkboxes):
+            if i != 1:
+                element.click()
 
-        driver.find_element(By.XPATH, '//*[@id="roster-details-multi-checkbox-modal-submit-button"]').click()
+        driver.find_element(By.ID, 'roster-details-multi-checkbox-modal-submit-button').click()
         time.sleep(1)
 
         # Get logins and add to logins dictionary
-        elements = driver.find_elements(By.CSS_SELECTOR, 'tbody tr td')
+        elements = driver.find_elements(By.CSS_SELECTOR, '.roster-details-table-body tr td')
         logins = set()
         for element in elements:
             if element.text != '':
